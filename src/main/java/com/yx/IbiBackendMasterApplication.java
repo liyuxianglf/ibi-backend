be9e6de23d1abd787d4c8fb4b9b34436479c52fd
lyx <1595367501@qq.com>
2023-11-12 15:37:38 +0000

    update src/main/java/com/yx/IbiBackendMasterApplication.java.
    Signed-off-by: lyx <1595367501@qq.com>

--- src/main/java/com/yx/IbiBackendMasterApplication.java
+++ src/main/java/com/yx/IbiBackendMasterApplication.java
@@ -4,7 +4,9 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * 项目启动类
+ */
 @EnableRabbit
 @SpringBootApplication
 public class IbiBackendMasterApplication {
